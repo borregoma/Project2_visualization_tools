295609b54fcfb240bbb9bca82bd6f04c7e6139dd
borregoma <mborrego57@gmail.com>
2021-07-24 17:55:53 -0700

    updating Pet with Map

---
+++ heroku/Starter/moma/__init__.py
@@ -0,0 +1,4 @@
+from moma.app import db
+
+# db.drop_all()
+db.create_all()
--- heroku/Starter/moma/models.py
+++ heroku/Starter/moma/models.py
@@ -1,13 +1,13 @@
 from .app import db
 
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class Map(db.Model):
+    __tablename__ = 'maps'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)      ##NEEDS TO BE UPDATED
     name = db.Column(db.String(64))
     lat = db.Column(db.Float)
     lon = db.Column(db.Float)
 
     def __repr__(self):
-        return '<Pet %r>' % (self.name)
+        return '<Map %r>' % (self.name)
